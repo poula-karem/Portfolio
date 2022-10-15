aae5c1b7837c3c3572e56ec1cff6882d2e6ebc3f
Paula-Karem <paulakaremp@gmail.com>
2022-10-16 00:53:02 +0200

    some changes

--- timer/script.js
+++ timer/script.js
@@ -15,6 +15,7 @@ start.addEventListener(`click`, function () {
     if (hou.value <= 0 && min.value <= 0 && sec.value <= 0
         || min.value > 60 || sec.value > 60) {
         flash(inputCard);
+        flashOff(inputCard);
         return false;
     }
 
